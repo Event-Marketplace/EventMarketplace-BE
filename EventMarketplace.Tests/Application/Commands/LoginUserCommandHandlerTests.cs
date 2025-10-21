d6fd46442400ae23d4c0812101903a15a6b4314a
Barteq14 <b.longota2@wp.pl>
2025-10-21 13:21:54 +0200

    added one positive test for login user command handler

--- EventMarketplace.Tests/Application/Commands/LoginUserCommandHandlerTests.cs
+++ EventMarketplace.Tests/Application/Commands/LoginUserCommandHandlerTests.cs
@@ -3,6 +3,7 @@ using EventMarketplace.Application.Commands.UserCommands.Handlers;
 using EventMarketplace.Application.Dtos.UserDtos;
 using EventMarketplace.Application.Exceptions;
 using EventMarketplace.Application.Patterns;
+using EventMarketplace.Application.Response;
 using EventMarketplace.Application.Utils;
 using EventMarketplace.Application.Utils.Jwt;
 using EventMarketplace.Domain.Entities;
@@ -36,13 +37,10 @@ public class LoginUserCommandHandlerTests
         _unitOfWork.Setup(x => x.Users).Returns(_userRepo.Object);
         _unitOfWork.Setup(x => x.BeginTransactionAsync(It.IsAny<CancellationToken>())).Returns(Task.CompletedTask);
         _unitOfWork.Setup(x => x.CommitAsync(It.IsAny<CancellationToken>())).Returns(Task.CompletedTask);
-        _user = new User()
-        {
-            EmailAddress = EmailAddress.Create("b.longota2@wp.pl"),
-            Password = "Password.122"
-        };
+        _user = new User();
     }
 
+    #region NegativeTests
 
     [Fact]
     public async Task Should_Throw_Exception_When_Email_Not_Exist()
@@ -54,7 +52,7 @@ public class LoginUserCommandHandlerTests
             new LoginUserDto()
             {
                 Email = "b.longota2@wp.pl",
-                Password = "Password.123"
+                Password = "Password.122"
             });
         
         //act
@@ -69,6 +67,7 @@ public class LoginUserCommandHandlerTests
     [Fact]
     public async Task Should_Throw_Exception_When_Password_Is_Wrong()
     {
+        _user.Password = "Password.122";
         _userRepo.Setup(x => x.GetUserByEmailAsync(It.IsAny<string>())).ReturnsAsync(_user);
         
         var command = new LoginUserCommand(
@@ -78,12 +77,48 @@ public class LoginUserCommandHandlerTests
                 Password = "Password.123"
             });
 
-        _passwordManager.Setup(x => x.ValidPassword(command.Dto.Password, _user.Password)).Returns(false);
+        _passwordManager.Setup(x => x.ValidPassword(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns((string plain, string hashed) => plain == hashed);
         
         Func<Task> action = () => _handler.ExecuteHandleAsync(command, CancellationToken.None);
 
         await Assert.ThrowsAsync<AppException>(action);
         _unitOfWork.Verify(x => x.CommitAsync(It.IsAny<CancellationToken>()), Times.Never);
         _unitOfWork.Verify(x => x.RollbackAsync(It.IsAny<CancellationToken>()), Times.Once);
+        _passwordManager.Verify(x => x.ValidPassword(command.Dto.Password, _user.Password), Times.Once);
     }
+
+    #endregion
+
+    #region PositiveTests
+
+    [Theory]
+    [InlineData("b.longota2@wp.pl", "Password.123", "qwertyuiop")]
+    [InlineData("b.longota@wp.pl", "Password.123", "qwertyuio")]
+    public async Task Login_Successfully_When_Valid_Credentials(string email, string password, string token)
+    {
+        //arrange
+        _user.Password = password;
+        _user.EmailAddress = EmailAddress.Create("b.longota2@wp.pl");
+        _userRepo.Setup(x => x.GetUserByEmailAsync(It.Is<string>(email => email == _user.EmailAddress.Value))).ReturnsAsync(_user);
+        _passwordManager.Setup(x => x.ValidPassword(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns((string p1, string p2) => p1 == p2);
+        _jwtProvider.Setup(x => x.GenerateToken(It.IsAny<User>())).Returns(token);
+
+        var command = new LoginUserCommand(
+            new LoginUserDto()
+            {
+                Email = email,
+                Password = password
+            });
+        //act
+        var result = await  _handler.ExecuteHandleAsync(command, CancellationToken.None);
+        
+        //asserts
+        Assert.Equal(token, result.TokenJwt);
+        _unitOfWork.Verify(x => x.CommitAsync(CancellationToken.None), Times.Once);
+        _unitOfWork.Verify(x => x.RollbackAsync(CancellationToken.None), Times.Never);
+    }
+
+    #endregion
 }
